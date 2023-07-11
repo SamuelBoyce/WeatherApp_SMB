dbebae9d506f0918e7915fab81675f0054e8461f
SamuelBoyce <samuel.boyce@gmail.com>
2023-07-11 09:53:59 -0400

    Initial Commit

---
+++ WeatherApp_SMB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp_SMB
+//
+//  Created by Samuel Boyce on 7/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
